4f222e944e14a419bcb7f79002bf74a0357a72ad
ajish <azisgodgod@gmail.com>
2025-02-20 10:35:11 +0700

    Perubahan pada header

--- building_web/core/urls.py
+++ building_web/core/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from core.views import index
+from core.views import index, contact, about
 
 urlpatterns = [
-    path ('', index),
+    path ('', index, name='index'),
+    path('contact/', contact, name='contact'),
+    path('about/', about, name='about'),
+
 ]
